d285702c5c7e8d8a6309a6feb369671e6e57b8de
java-instructor <java-instructor@example.com>
2022-04-14 16:15:47 +0900

    update code

--- workspace/java-basic/examples-c/src/Ex04Generic.java
+++ workspace/java-basic/examples-c/src/Ex04Generic.java
@@ -1,13 +1,14 @@
 
+// 클래스에 포함된 어떤 변수에 다양한 자료형의 데이터를 저장하는 방법
+
 class TheObject {
-	int a; // a 변수에는 정수만 저장할 수 있습니다.
-	Object o; // 모든 자료형의 데이터를 저장할 수 있습니다.
+	int a; 		// a 변수에는 정수만 저장할 수 있습니다.
+	Object o; 	// 모든 자료형의 데이터를 저장할 수 있습니다.
 }
 
 class TheGeneric<E> { // 아직 확정되지 않은 E자료형을 사용하는 클래스 ( 변수 선언할 때 자료형 확정 )
 	E o;
 }
-
 class TheGeneric2<E, O> {
 	E a1;
 	O a2;
@@ -16,24 +17,9 @@ class TheGeneric2<E, O> {
 	E m2() { return null; }
 	O m3() { return null; }
 }
-
 public class Ex04Generic {
 
-	public static void main(String[] args) {
-		
-		TheObject obj1 = new TheObject();
-		obj1.a = 10;
-		// obj1.a = "Hello"; // a 변수는 정수만 저장할 수 있습니다.
-		obj1.o = 100;
-		obj1.o = "Hello"; // o 변수는 모든 자료형의 데이터를 저장할 수 있습니다.
-		
-		int a2 = obj1.a; // 자료형이 고정된 변수에서 데이터를 읽는 경우 형변환 불필요
-		String s = (String)obj1.o; // Object에 저장된 변수를 읽을 때에는 형변환 필요
-		// a2 = (int)obj1.o; // 형변환을 잘못하면 오류 발생
-		
-		System.out.printf("%d, %s\n", a2, s);
-		
-		//////////////////////////////////////////////
+	public static void main(String[] args) {		
 		
 		TheGeneric<String> g = new TheGeneric<String>(); // 변수 만들때 자료형 결정
 		g.o = "Hello";	
