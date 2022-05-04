77d3f5566c7e9cd0a69aa5f0bc4ec79f86a793bd
java-instructor <java-instructor@example.com>
2022-05-04 17:13:10 +0900

    add file

--- workspace/db/jdbc-basic/src/LottoNumberScraper.java
+++ workspace/db/jdbc-basic/src/LottoNumberScraper.java
@@ -5,15 +5,15 @@ import org.jsoup.select.Elements;
 
 public class LottoNumberScraper {
 
-	public void doScrap() {
+	public void doScrap(String round) {
 		
 		try {
 			// 지정된 주소로 요청을 보내고 응답 수신 ( 응답은 html )
 //			Document doc = Jsoup.connect("https://dhlottery.co.kr/gameResult.do?method=byWin")
 //					   			.get(); // get 방식 요청
 			Document doc = Jsoup.connect("https://dhlottery.co.kr/gameResult.do?method=byWin")
-								.data("drwNo", "1004")
-								.data("drwNoList", "1004")
+								.data("drwNo", round)
+								.data("drwNoList", round)
 								.post(); // post 방식 요청
 			
 			// System.out.println(doc.body().text());
@@ -24,7 +24,7 @@ public class LottoNumberScraper {
 //										   "div.num.win > p > span");
 			Elements elements = doc.select(".ball_645");
 			
-			System.out.print("[WINNING NUMBERS] : ");
+			System.out.printf("[WINNING NUMBERS ( ROUND %4s )] : ", round);
 			for (Element element : elements) {
 				System.out.printf("[%2s]", element.text());
 			}
@@ -39,7 +39,12 @@ public class LottoNumberScraper {
 	public static void main(String[] args) {
 		
 		LottoNumberScraper scraper = new LottoNumberScraper();
-		scraper.doScrap();
+		for (int round = 500; round <= 510; round++) {
+			// String.valueOf(data) -> data를 문자열로 변경
+			scraper.doScrap(String.valueOf(round));
+			
+			try { Thread.sleep(1000); } catch (Exception ex) {}
+		}
 		
 
 	}
