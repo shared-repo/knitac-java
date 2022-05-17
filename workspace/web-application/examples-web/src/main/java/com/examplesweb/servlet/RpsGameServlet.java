60fb580ae6c9dc16a988a94a2b4e60c583833771
java-instructor <java-instructor@example.com>
2022-05-17 15:11:37 +0900

    update code

--- workspace/web-application/examples-web/src/main/java/com/examplesweb/servlet/RpsGameServlet.java
+++ workspace/web-application/examples-web/src/main/java/com/examplesweb/servlet/RpsGameServlet.java
@@ -18,13 +18,31 @@ public class RpsGameServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// 1. 요청 데이터 읽기
 		String user = req.getParameter("myselection");
+		// 삼항연산자 : 항1 ? 항2 : 항3 --> 항1이 true 이면 항2 선택 아니면 항3 선택
+		int iUser = user.equals("scissors") ? 0 : ( user.equals("rock") ? 1 : 2 ) ;
+		String kUser = user.equals("scissors") ? "가위" : ( user.equals("rock") ? "바위" : "보" ) ;
 		
+		//2. 요청 처리 ( 가위 바위 보 게임 실행 )
+		int com = (int)(Math.random() * 3); // 0(가위), 1(바위), 2(보)
+		
+		String result = "";
+		if (iUser == com) {
+			result = "비겼습니다.";
+		} else if ((iUser == 0 && com == 2) ||
+				   (iUser == 1 && com == 0) ||
+				   (iUser == 2 && com == 1)) {
+			result = "이겼습니다.";
+		} else {
+			result = "졌습니다.";
+		}		
 		
 		// 3. 응답 컨텐츠 생산 + 응답
 		resp.setContentType("text/html;charset=utf-8");
 		PrintWriter out = resp.getWriter();
 		
-		out.write("당신의 선택은 : " + user + " 입니다.");
+		out.write("당신의 선택은 : " + kUser + " 입니다.");
+		out.write("<br>");
+		out.write("결과는 " + result);
 		
 	}
 	
