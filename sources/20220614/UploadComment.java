dfa2952006b004a19f3ef6524d6de3c592a5a6be
java-instructor <java-instructor@example.com>
2022-06-14 09:53:41 +0900

    add files

---
+++ sources/20220614/UploadComment.java
@@ -0,0 +1,18 @@
+package com.demoweb.dto;
+
+import java.util.Date;
+
+
+public class BoardComment {
+
+	private int commentNo;
+	private int boardNo;
+	private String writer;
+	private String content;
+	private Date regDate;
+	
+	private int groupNo;
+	private int step;
+	private int depth;
+	
+}
