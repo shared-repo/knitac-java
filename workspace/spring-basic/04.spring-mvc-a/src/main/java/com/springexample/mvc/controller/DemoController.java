005b97c184d54961deeb7fdae64264d33fcba775
java-instructor <java-instructor@example.com>
2022-06-20 10:42:07 +0900

    add files

--- workspace/spring-basic/04.spring-mvc-a/src/main/java/com/springexample/mvc/controller/DemoController.java
+++ workspace/spring-basic/04.spring-mvc-a/src/main/java/com/springexample/mvc/controller/DemoController.java
@@ -3,7 +3,10 @@ package com.springexample.mvc.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -49,12 +52,37 @@ public class DemoController {
 //	}
 	
 	//5. 전달 인자로 직접 요청 데이터 수신 ( 지정된 전달인자에 해당하는 요청 데이터가 없는 경우 )
-	@RequestMapping(path = { "/demo/param.action" })
+//	@RequestMapping(path = { "/demo/param.action" })
+//	public String processGetRequest(String data1, 
+//									int data2,
+//									@RequestParam(defaultValue = "0") int data3) { // 요청 데이터에 없는 전달인자
+//		
+//		System.out.println(data1 + " / " + data2 + " / " + data3);
+//		
+//		return "result"; // "/WEB-INF/views/" + result + ".jsp"
+//	}
+	
+	//6. method 속성으로 요청 구분 ( get 요청 수신 )
+	// @RequestMapping(path = { "/demo/param.action" }, method = RequestMethod.GET)
+	@GetMapping(path = { "/demo/param.action" })
 	public String processGetRequest(String data1, 
 									int data2,
 									@RequestParam(defaultValue = "0") int data3) { // 요청 데이터에 없는 전달인자
 		
 		System.out.println(data1 + " / " + data2 + " / " + data3);
+		
+		return "result"; // "/WEB-INF/views/" + result + ".jsp"
+	}
+	
+	//7. method 속성으로 요청 구분 ( post 요청 수신 )
+	// @RequestMapping(path = { "/demo/param.action" }, method = RequestMethod.POST)
+	@PostMapping(path = { "/demo/param.action" })
+	public String processPostRequest(String name, 
+									 String phone,
+									 String email, 
+									 int age) {
+		
+		System.out.printf("[%s][%s][%s][%d]\n", name, phone, email, age);
 		
 		return "result"; // "/WEB-INF/views/" + result + ".jsp"
 	}
