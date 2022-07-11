0a12db3acb13e9ddd1bb7b67d25bddce4f150cca
java-instructor <java-instructor@example.com>
2022-07-11 09:44:07 +0900

    add files

--- workspace/spring-basic/spring-demoweb4/src/main/java/com/demoweb/controller/BoardController.java
+++ workspace/spring-basic/spring-demoweb4/src/main/java/com/demoweb/controller/BoardController.java
@@ -36,23 +36,9 @@ public class BoardController {
 	private BoardService boardService;
 
 	@GetMapping(path = { "/list" })
-	public String list(@RequestParam(defaultValue = "1")int pageNo, 
-					   Model model) {
+	public String list() {
 		
-		int pageSize = 3; // 한 페이지에 표시할 데이터 개수
-		int pagerSize = 3; // 표시되는 페이지 번호 개수 ( 보이지 않은 페이지 번호는 다음, 이전 등으로 표시 )
-		int count = 0; // 전체 데이터 개수	
 		
-		// List<Board> boardList = boardService.findAll();
-		List<Board> boardList = boardService.findByPage(pageNo, pageSize);		
-		count = boardService.findBoardCount(); // 데이터베이스에 전체 개시물 개수 조회	
-		
-		ThePager pager = new ThePager(count, pageNo, pageSize, pagerSize, "list");		
-		
-		// Model 타입의 전달인자에 데이터를 저장하면 View(JSP)로 데이터가 전달됩니다. ( request에 저장 )
-		model.addAttribute("boardList", boardList);
-		model.addAttribute("pager", pager);
-		model.addAttribute("pageNo", pageNo);
 		
 		return "board/list";  // --> /WEB-INF/views/ + board/list + .jsp
 	}
